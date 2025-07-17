b27469da2dfb91daa8106f018a506ce7cad57b08
Lucas Tavares <ltfreire@stefanini.com>
2025-07-17 16:37:50 -0300

    feat: update readme

--- bluecard-fed/src/store/auth.ts
+++ bluecard-fed/src/store/auth.ts
@@ -44,7 +44,8 @@ export const useAuthStore = defineStore("auth", {
     async login(email: string, password: string) {
       const response = await api.post("/auth/login", { email, password });
       this.token = response.data.token;
-      localStorage.setItem("token", this.token!);
+      if (this.token) localStorage.setItem("token", this.token);
+
       await this.fetchUserFromToken();
     },
 
